ee820090166d1c11e113cbc620823f88f28f9dee
b9nn <bgladney02@gmail.com>
2026-05-13 09:58:21 -0400

    Overload heapq.merge/nlargest/nsmallest on key=None

--- stdlib/heapq.pyi
+++ stdlib/heapq.pyi
@@ -1,8 +1,8 @@
 import sys
 from _heapq import *
-from _typeshed import SupportsRichComparison
+from _typeshed import SupportsRichComparison, SupportsRichComparisonT
 from collections.abc import Callable, Generator, Iterable
-from typing import Any, Final, TypeVar
+from typing import Any, Final, TypeVar, overload
 
 __all__ = ["heappush", "heappop", "heapify", "heapreplace", "merge", "nlargest", "nsmallest", "heappushpop"]
 
@@ -14,9 +14,18 @@ _S = TypeVar("_S")
 
 __about__: Final[str]
 
+@overload
 def merge(
-    *iterables: Iterable[_S], key: Callable[[_S], SupportsRichComparison] | None = None, reverse: bool = False
-) -> Generator[_S]: ...
-def nlargest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison] | None = None) -> list[_S]: ...
-def nsmallest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison] | None = None) -> list[_S]: ...
+    *iterables: Iterable[SupportsRichComparisonT], key: None = None, reverse: bool = False
+) -> Generator[SupportsRichComparisonT]: ...
+@overload
+def merge(*iterables: Iterable[_S], key: Callable[[_S], SupportsRichComparison], reverse: bool = False) -> Generator[_S]: ...
+@overload
+def nlargest(n: int, iterable: Iterable[SupportsRichComparisonT], key: None = None) -> list[SupportsRichComparisonT]: ...
+@overload
+def nlargest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison]) -> list[_S]: ...
+@overload
+def nsmallest(n: int, iterable: Iterable[SupportsRichComparisonT], key: None = None) -> list[SupportsRichComparisonT]: ...
+@overload
+def nsmallest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison]) -> list[_S]: ...
 def _heapify_max(heap: list[Any], /) -> None: ...  # undocumented
